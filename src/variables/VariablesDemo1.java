b665a882f6f9fabcb32a15ab99b6c0a6a48532d0
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2025-01-23 19:47:40 +0530

    Java Season 1 notes and programs till Jan 23rd

--- src/variables/VariablesDemo1.java
+++ src/variables/VariablesDemo1.java
@@ -12,6 +12,11 @@ public class VariablesDemo1 {
 		
 		System.out.println(x);
 		System.out.println(y);
+		//  System.out.println(a);  ==> Cannot make a static reference to the non-static field a
+		
+		VariablesDemo1 obj=new VariablesDemo1();
+		System.out.println(obj.a);
+		System.out.println(obj.b);
 
 	}
 	
---
+++ src/variables/VariablesDemo2.java
@@ -0,0 +1,14 @@
+package variables;
+
+public class VariablesDemo2 {
+
+	static int j;
+	
+	public static void main(String[] args) {
+		
+		System.out.println(VariablesDemo2.j);
+		
+
+	}
+
+}
